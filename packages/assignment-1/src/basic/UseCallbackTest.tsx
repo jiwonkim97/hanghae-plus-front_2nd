ec4422ba6c2dba446f8b43f3e4419d4212d3dbf2
jiwon <kimjiwon97@gmail.com>
2024-06-18 23:38:23 +0000

    UseCallbackTest 해결

--- packages/assignment-1/src/basic/UseCallbackTest.tsx
+++ packages/assignment-1/src/basic/UseCallbackTest.tsx
@@ -1,24 +1,27 @@
-import { useCallback, useState } from "react";
+import { memo, useCallback, useState } from "react";
 import { BarkButton, MeowButton } from "./UseCallbackTest.components.tsx";
 
 export default function UseCallbackTest() {
   const [meowCount, setMeowCount] = useState(0);
   const [barkedCount, setBarkedCount] = useState(0);
 
-  const Meow = useCallback(() => {
-    return <p data-testid="cat">meowCount {meowCount}</p>
-  }, [meowCount])
-  
-  const Bark = useCallback(() => {
-    return <p data-testid="dog">barkedCount {barkedCount}</p>
-  }, [barkedCount])
+  const handleMeow = useCallback(() => {
+    setMeowCount(n => n + 1)
+  }, [])
+
+  const handleBark = useCallback(() => {
+    setBarkedCount(n => n + 1)
+  }, [])
 
   return (
     <div>
-      <Meow />
-      <Bark />
-      <MeowButton onClick={() => setMeowCount(n => n + 1)}/>
-      <BarkButton onClick={() => setBarkedCount(n => n + 1)}/>
+      <Meow meowCount={meowCount}/>
+      <Bark barkedCount={barkedCount}/>
+      <MeowButton onClick={handleMeow}/>
+      <BarkButton onClick={handleBark}/>
     </div>
   );
 }
+
+const Meow = memo(({meowCount}:{meowCount: number}) => <p data-testid="cat">meowCount {meowCount}</p>)
+const Bark = memo(({barkedCount}:{barkedCount: number}) => <p data-testid="dog">barkedCount {barkedCount}</p>)
