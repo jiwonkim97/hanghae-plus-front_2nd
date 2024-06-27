b543443491cae6835f947f40c57634adf4bd0b46
jiwon <kimjiwon97@gmail.com>
2024-06-27 13:41:32 +0000

    추가 과제 머지 중 추가된 js 파일들 삭제

--- packages/assignment-2/src/advanced/advanced.jsx
+++ packages/assignment-2/src/advanced/advanced.jsx
@@ -1,6 +1,16 @@
 import { createContext, useContext, useState } from "react";
 
-export const memo1 = (fn) => fn();
+const cache = new Map();
+
+export const memo1 = (fn) => {
+  if (cache.has(fn)) {
+    return cache.get(fn)
+  };
+  const ret = fn()
+  cache.set(fn, ret);
+
+  return ret;
+};
 
 export const memo2 = (fn) => fn();
 
