33fba10f71c05f10ef7bc392327169c04ba2e026
jiwon <kimjiwon97@gmail.com>
2024-07-02 13:54:56 +0000

    테스트 코드 추가, hooks.js 완료, MyReact fail 발생(에효..)

--- packages/assignment-3/src/basic/hooks.js
+++ packages/assignment-3/src/basic/hooks.js
@@ -1,33 +1,57 @@
 import { deepEquals } from '../../../assignment-2/src/basic/basic';
 
 export function createHooks(callback) {
-  let _state = new Map()
+  /**
+   * let _state = new Map()
+   * 기존에 위와 같은 형식으로 initState 기반의 key를 통해 값을 저장했다.
+   * 초기의 테스트코드는 문제없이 통과하지만,
+   * 새로 추가한 "동일한 initState를 가진 여러개의 state를 선언해도 각각 다른 상태를 갖는다."
+   * 테스트 실행 시 테스트를 통과하지 못한다.
+   * 
+   * Math.random()함수로 테스트 했을 시에는 매번 random값이 바뀌는 이슈가 있었다.
+   * 왜지?
+   */
+  let stateList = [];
+  let pState = -1;
   const useState = (initState) => {
-    // 더 나은 키가 필요함
-    let key = initState
-    if(_state.get(key) === undefined){
-      _state.set(key, initState)
-    }
+    // 중복이 되지 않을 uuid가 필요함
+    stateList.push(initState);
+    pState += 1;
+    const currentIndex = pState;
+
+    const state = stateList[currentIndex];
+
     const setState = (newState) => {
-      if(deepEquals(_state.get(key), newState)){
-        return
-      }else{
+      if (!deepEquals(stateList[currentIndex], newState)) {
         console.log("state changed!!!")
-        console.log("before: ", _state.get(key))
+        console.log("before: ", stateList[currentIndex])
         console.log("after: ", newState)
-        _state.set(key, newState)
+        console.log("============================")
+        stateList[currentIndex] = newState
         callback()
       }
     }
-    return [_state.get(key), setState];
+    return [state, setState];
   };
 
+  let memoList = [];
+  let pMemo = -1;
   const useMemo = (fn, refs) => {
-    return fn();
+    let target = fn();
+    memoList.push({ value: target, ref: [...refs] })
+    pMemo += 1;
+
+    const currentIndex = pMemo;
+
+    if (!deepEquals(memoList[currentIndex].ref, [...refs])) {
+      memoList[currentIndex] = { value: fn(), ref: [...refs] }
+    }
+    return memoList[currentIndex].value;
   };
 
   const resetContext = () => {
-
+    pState = -1
+    pMemo = -1
   }
 
   return { useState, useMemo, resetContext };
