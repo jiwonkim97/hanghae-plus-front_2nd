d113725f6757a04b0c2109bf2c43c2cb8755d20b
jiwon <kimjiwon97@gmail.com>
2024-07-02 03:26:06 +0000

    hooks.js 풀이중중

--- packages/assignment-3/src/basic/hooks.js
+++ packages/assignment-3/src/basic/hooks.js
@@ -1,23 +1,25 @@
 import { deepEquals } from '../../../assignment-2/src/basic/basic';
 
 export function createHooks(callback) {
-  let _state = null
+  let _state = new Map()
   const useState = (initState) => {
-    if(_state === null ){
-      _state = initState
+    // 더 나은 키가 필요함
+    let key = initState
+    if(_state.get(key) === undefined){
+      _state.set(key, initState)
     }
     const setState = (newState) => {
-      if(deepEquals(_state, newState)){
+      if(deepEquals(_state.get(key), newState)){
         return
       }else{
         console.log("state changed!!!")
-        console.log("before: ", _state)
+        console.log("before: ", _state.get(key))
         console.log("after: ", newState)
-        _state = newState
+        _state.set(key, newState)
         callback()
       }
     }
-    return [_state, setState];
+    return [_state.get(key), setState];
   };
 
   const useMemo = (fn, refs) => {
