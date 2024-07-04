5a5abcd02d948bf28f0ea3655c7cda4ddea41959
jiwon <kimjiwon97@gmail.com>
2024-07-04 13:03:41 +0000

    Basic test 통과

--- packages/assignment-3/src/basic/MyReact.js
+++ packages/assignment-3/src/basic/MyReact.js
@@ -1,5 +1,5 @@
 import { createHooks } from "./hooks";
-import { render as customRender } from "./render";
+import { render as customRender, isNull } from "./render";
 
 function MyReact() {
   let _root = null;
@@ -13,23 +13,23 @@ function MyReact() {
     resetHookContext()
   }
   const _render = () => {
-    console.log("rerender")
-    console.log(JSON.stringify(_oldComponent))
-    console.log(JSON.stringify(_rootComponent()))
-    console.log("==============================")
+    resetHookContext()
+    const newComponent = _rootComponent()
 
-    customRender(_root, _rootComponent(), _oldComponent)
-    _oldComponent = _rootComponent()
+    if (isNull(_oldComponent)) {
+      customRender(_root, newComponent)
+    } else {
+      customRender(_root, newComponent, _oldComponent)
+    }
+    _oldComponent = newComponent
   };
+
   function render($root, rootComponent) {
-    console.log("render")
-    console.log($root.innerHTML)
-    console.log(JSON.stringify(rootComponent()))
-    console.log("==============================")
+    resetContext()
     _root = $root
     _rootComponent = rootComponent
-    _oldComponent = rootComponent()
-    customRender($root, rootComponent())
+
+    _render()
   }
 
   const { useState, useMemo, resetContext: resetHookContext } = createHooks(_render);
