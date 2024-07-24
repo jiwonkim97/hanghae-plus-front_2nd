004526b83d8fc6ef6cdb0814cf7072c355d24ecf
AquilaHash-Jiwon <kimjiwon97@gmail.com>
2024-07-25 00:22:33 +0900

    change coverage file location, read logic

--- packages/assignment-6/vite.config.ts
+++ packages/assignment-6/vite.config.ts
@@ -5,12 +5,17 @@ import react from '@vitejs/plugin-react-swc';
 export default mergeConfig(
   defineConfig({
     plugins: [react()],
+    test: {
+      coverage: {
+        provider: 'v8'
+      }
+    }
   }),
   defineTestConfig({
     test: {
       globals: true,
       environment: 'jsdom',
-      setupFiles: './src/setupTests.ts'
+      setupFiles: './src/setupTests.ts',
     },
   })
 )
