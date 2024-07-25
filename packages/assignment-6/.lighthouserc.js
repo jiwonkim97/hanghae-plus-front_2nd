9f39cc88fb8a3b3b3fe0503dddd3c727d9a8ec95
AquilaHash-Jiwon <kimjiwon97@gmail.com>
2024-07-25 20:35:07 +0900

    fix .lighthouserc.js, add yml job, package.json script

--- packages/assignment-6/.lighthouserc.js
+++ packages/assignment-6/.lighthouserc.js
@@ -4,6 +4,9 @@ module.exports = {
       startServerCommand: "pnpm -F assignment-6 start",
       url: ["http://localhost:3000"],
       numberOfRuns: 5,
+      settings: {
+        preset: "desktop",
+      },
     },
     upload: {
       target: "filesystem",
