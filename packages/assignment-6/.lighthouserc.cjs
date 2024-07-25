534f51f1ddc6702136ca9d4017b1004068952d88
AquilaHash-Jiwon <kimjiwon97@gmail.com>
2024-07-25 20:41:28 +0900

    change lighthouse port

--- packages/assignment-6/.lighthouserc.cjs
+++ packages/assignment-6/.lighthouserc.cjs
@@ -2,7 +2,7 @@ module.exports = {
   ci: {
     collect: {
       startServerCommand: "pnpm -F assignment-6 start",
-      url: ["http://localhost:3000"],
+      url: ["http://localhost:8080"],
       numberOfRuns: 5,
       settings: {
         preset: "desktop",
